6379c7ff419174af4fd5407120d8935317224485
SandroBarillaPXL <sandrobarilla2001@gmail.com>
2024-12-03 13:57:46 +0100

    add reload scripts

---
+++ reload.ps1
@@ -0,0 +1,4 @@
+docker container stop terminal
+docker container rm terminal
+docker image rm terminal-terminal
+docker compose up -d
---
+++ reload.sh
@@ -0,0 +1,4 @@
+docker compose down
+docker container rm terminal
+docker image rm terminal-terminal
+docker compose up -d
